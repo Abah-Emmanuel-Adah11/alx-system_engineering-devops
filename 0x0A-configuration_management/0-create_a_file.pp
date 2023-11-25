90af18db90c88021b178531c2991db6af461be04
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-11-25 07:13:45 -0800

    Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# creates a file in /tmp
+# A code that creates a file in /tmp
 file { '/tmp/school':
+  ensure  => file,
   content => 'I love Puppet',
-  mode	  => '0744',
+  mode	  => '07744',
   owner	  => 'www-data',
   group   => 'www-data',
 }
